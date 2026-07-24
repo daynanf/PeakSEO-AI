c05076220a09b6f556669019dee494eb4c2d4423
daynan f <dinuzeynu22@gmail.com>
2026-07-24 16:41:08 +0300

    feat: add navbar sample

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import {Link, useNavigate, useLocation} from "react-router-dom";
+
+export default function Navbar(){
+    const {user}={user:{name: "John",email:"john@example.com", plan:"free"}};
+    
+    
+    
+    return (
+        <div>
+            <h1>Navbar</h1>
+        </div>
+    )   
+}

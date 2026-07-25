31416f2af85c6a7c4d1d3ee46c042336f44dc3c3
daynan f <dinuzeynu22@gmail.com>
2026-07-25 14:18:55 +0300

    feat:add theme context and custom hook for theme managment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 export default function App(){
     return (
-        <Home/>
+        <>
+            <Navbar/>
+            <Home/>
+        </>
+        
     )
 }

bab5e0cdecac76a0a31cc1e2d00cae0d70cd9d4c
daynan f <dinuzeynu22@gmail.com>
2026-07-27 15:47:16 +0300

    feat: add pricing section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Features from "../components/home/Features";
 import { Hero } from "../components/home/Hero";
 import HowItWorks from "../components/home/HowItWorks";
+import Pricing from "../components/home/Pricing";
 import Navbar from "../components/Navbar";
 
 export default function Home(){   
@@ -10,5 +11,6 @@ export default function Home(){
             <Hero/>
             <Features/>
             <HowItWorks/>
+            <Pricing/>
         </>
     )}

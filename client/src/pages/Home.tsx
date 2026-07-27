832bd33647f55c9c0f56426800704813368627d2
daynan f <dinuzeynu22@gmail.com>
2026-07-27 16:08:47 +0300

    feat: add footer section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Features from "../components/home/Features";
 import { Hero } from "../components/home/Hero";
 import HowItWorks from "../components/home/HowItWorks";
 import Pricing from "../components/home/Pricing";
+import Footer from "../components/home/Footer";
 import Navbar from "../components/Navbar";
 
 export default function Home(){   
@@ -12,5 +13,6 @@ export default function Home(){
             <Features/>
             <HowItWorks/>
             <Pricing/>
+            <Footer/>
         </>
     )}

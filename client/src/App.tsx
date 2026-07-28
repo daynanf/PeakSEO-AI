5b7608e443e510f7778a4220f5ca5827f201baee
daynan f <dinuzeynu22@gmail.com>
2026-07-28 11:24:22 +0300

    feat: add react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+
 
 export default function App(){
+     const location = useLocation();
+
+    const hideNavbar = ["/login", "/register"].includes(location.pathname);
+
     return (
-        <>
-            <Navbar/>
-            <Home/>
+        <> 
+            <Toaster/>
+            {!hideNavbar && <Navbar/>}
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/login" element={<Login state="login" />} />
+                <Route path="/register" element={<Login state="register"/>} />
+            </Routes>
         </>
         
     )

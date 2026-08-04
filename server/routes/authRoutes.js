2c8415105b69124b3888ce06fde443afd91da46c
daynan f <dinuzeynu22@gmail.com>
2026-08-04 16:54:49 +0300

    feat: add auth router

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { login, register, getUser } from "../controllers/authController.js";
+
+const authRouter = express.Router();
+
+authRouter.post('/register',register);
+authRouter.post('/login',login);
+authRouter.get('/user', auth, getUser);
+
+export default authRouter;

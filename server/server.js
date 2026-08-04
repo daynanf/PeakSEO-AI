9b28754a1134fc42a6139406d83f0da256397912
daynan f <dinuzeynu22@gmail.com>
2026-08-04 16:58:05 +0300

    fix: import middleware in authRouter

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ connectDB();
 const app = express()
 
 app.use(cors())
+app.use("/api/auth", authRouter)
 app.use(express.json())
 
 app.get('/', (req,res)=>res.send("Server is running"))

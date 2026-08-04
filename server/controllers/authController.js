9087e944859e218ec851a368ee22fa5b360ca61a
daynan f <dinuzeynu22@gmail.com>
2026-08-04 12:35:41 +0300

    feat: add getUser funciton for auth controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -45,3 +45,13 @@ export const login = async(req,res)=>{
 }
 
 
+export const getUser =async(req,res)=>{
+    try {
+        const user = await User.findById(req.userId).select("-password")
+        if (!user) return res.status(404).json({success: false, message: "User not found"})
+        res.status(200).json({success: true, user})
+    } catch (error) {
+        console.error("Get user error", error.message)
+        res.status(500).json({success: false, message: "Server error"})
+    }
+}
